20ef3f518d1c9a4efc13dce9a6989364007eba67
superbigcup325 <2025150125@mails.szu.edu.cn>
2026-05-23 14:21:20 +0800

    update

--- platform/luogu/p8622/solution.cpp
+++ platform/luogu/p8622/solution.cpp
@@ -6,6 +6,12 @@ using namespace std;
 const int mod = 1e9+7;
 const int N = 1e5+5;
 
+// 数据范围：1 < L < R < N < 1e15
+// 那么只需要看 [L, R] 的情况
+// 对于第 i 个， 他的因数肯定在之前出现过，那么 i 的状态只取决于因数的奇偶性
+// 又显然，仅有完全平方数存在奇数个因数
+// 那么我们可以将问题转化为统计 [L, R] 区间内的完全平方数个数
+
 signed main() {
     int n,l,r;
     cin>>n>>l>>r;
